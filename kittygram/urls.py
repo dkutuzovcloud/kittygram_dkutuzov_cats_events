914089d6cae9a95f80828ed9d6ebf871cc0eda91
dkutuzovcloud <danila.kutuzov.79@mail.ru>
2026-04-13 22:25:46 +0300

    create api docs

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from cats import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # JWT
 from rest_framework_simplejwt.views import (
@@ -13,11 +16,26 @@ router = DefaultRouter()
 router.register(r'cats', views.CatViewSet)
 router.register(r'events', views.EventViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Kittygram API",
+        default_version='v1',
+        description="Документация API кото-ивентов",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 
-    # JWT авторизация
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # JWT
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+
+    # 📄 ДОКУМЕНТАЦИЯ
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
